0c50a5b28b150253b1b1bb683c4bff0d88b1ec8b
Dean Hatch <deanha@microsoft.com>
2018-02-23 15:15:39 -0800

    updating room descriptions

--- rooms.py
+++ rooms.py
@@ -10,14 +10,18 @@ class room():
 
 
 entry_description = """You look around and see that you are in a dimmly lit room with stone walls.
-                        at first the room apears empty but as your eyes adjust, you spy a fuzzy square outline on the floor 
-                        on the west side. """
+at first the room apears empty but as your eyes adjust, you spy a fuzzy square outline on the floor 
+on the west side. """
 
 hallway_description = """You make your way into the darkened hallway.
-                        The walls are rought stone, as though this is more a tunnel than a hallway.
-                        On the dirt floor ahead of you, a small glint briefly catches your eye"""
+The walls are rought stone, as though this is more a tunnel than a hallway.
+On the dirt floor ahead of you, a small glint briefly catches your eye"""
 
 chamber_description = """As you enter the chamber you see a large round room.
-                        The walls are smooth stone, with many harnesses and strange metal instruments attached to them.
-                        On the floor staw is spread out in the far quarter. 
-                        Two thick wooden benches are crowded together to your left."""
+The walls are smooth stone, with many harnesses and strange metal instruments attached to them.
+On the floor staw is spread out in the far quarter. 
+Two thick wooden benches are crowded together to your left."""
+
+entry_room = room("Entry", entry_description, "west")
+hallway = room("Hallway", hallway_description, "East")
+chamber = room("Chamber", chamber_description, "West")
